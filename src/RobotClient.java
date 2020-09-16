daa66849dee0c9db0746f1f8b08f834e4f829452
ojb7 <44772093+ojb7@users.noreply.github.com>
2020-09-16 11:52:16 +0200

    En kommentar

--- src/RobotClient.java
+++ src/RobotClient.java
@@ -6,6 +6,7 @@ public class RobotClient {
 
     private static final String HOST = "10.22.192.92";
     private static final int PORT = 42069;
+    //HELOOOOOOO
 
     public static void main(String[] args) throws IOException {
         imageSender = new ImageSender(HOST, PORT);
