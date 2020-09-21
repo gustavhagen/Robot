a64adad955e0b98596c952fc102c473831f9747d
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-09-21 19:14:12 +0200

    SendImage2

--- src/ImageSender2.java
+++ src/ImageSender2.java
@@ -2,18 +2,28 @@ import java.io.*;
 import java.net.Socket;
 
 public class ImageSender2 {
+
+    private final static String HOST = "83.243.218.40";
+    private final static int PORT = 42069;
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        Socket socket = new Socket("83.243.218.40", 42069);
+        System.out.println("Connecting to server...");
+        Socket socket = new Socket(HOST, PORT);
+        System.out.println("Connected to server on: " + HOST + ":" + PORT);
         OutputStream outputStream = socket.getOutputStream();
 
-        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("C:\\Users\\gusta\\OneDrive\\Skrivebord\\images"));
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("C:\\Users\\Gustav\\Desktop\\images\\sau.jpg"));
+        //File file = new File("C:\\Users\\Gustav\\Desktop\\images\\sau.jpg");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
         Object object = objectInputStream.readObject();
 
-
-        while(!socket.isClosed()){
+        int imageCounter = 0;
+        while(!socket.isClosed() & (imageCounter < 4)){
             objectOutputStream.writeObject(object);
+            //int imageSize = (int)file.length();
+            //System.out.println(imageSize);
+            imageCounter++;
         }
     }
 }
