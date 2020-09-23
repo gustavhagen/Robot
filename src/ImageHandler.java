536397d60f418cc68ce7173df986850e280c448e
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-09-23 10:42:34 +0200

    We made BIIIIIG progress

---
+++ src/ImageHandler.java
@@ -0,0 +1,41 @@
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.Socket;
+
+public class ImageHandler{
+
+
+    private final static String HOST = "10.22.192.92";
+    private final static int PORT = 42069;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+            System.out.println("Connecting to server...");
+            Socket socket = new Socket(HOST, PORT);
+            System.out.println("Connected to server on: " + HOST + ":" + PORT);
+
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+            ImageObject imageObject = null;
+
+            int imageCount = 0;
+            while(!socket.isClosed() & (imageCount < 10)){
+                File image = new File("C:\\Users\\Gustav\\Desktop\\images\\sau1.jpg");
+                byte[] byteArray = imageToByteArray(image);
+                imageObject = new ImageObject("Image"+imageCount, image.length(), byteArray, "23.09.2020", "jpg");
+                objectOutputStream.writeObject(imageObject);
+                System.out.println("Image was sent!");
+                // Thread.sleep(2000);
+                imageCount++;
+            }
+    }
+
+    private static final byte[] imageToByteArray(File image) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(image);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "jpg", byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+}
---
+++ src/ImageObject.java
@@ -0,0 +1,38 @@
+import java.io.Serializable;
+
+public class ImageObject implements Serializable {
+
+    private String name = null;
+    private long size = 0;
+    private byte[] imageBytes = new byte[2*14];
+    private String date = null;
+    private String filetype = null;
+
+    public ImageObject(String name, long size, byte[] imageBytes, String date, String filetype) {
+        this.name = name;
+        this.size = size;
+        this.imageBytes = imageBytes;
+        this.date = date;
+        this.filetype = filetype;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public byte[] getImageBytes() {
+        return imageBytes;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getFiletype() {
+        return filetype;
+    }
+}
