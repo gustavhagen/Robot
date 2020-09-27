cec2260f1689a919738378cc399ccb59465264bd
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-09-27 17:36:03 +0200

    Satt opp pins for Drive.java

--- src/Drive.java
+++ src/Drive.java
@@ -4,6 +4,16 @@ import com.pi4j.io.gpio.*;
 public class Drive {
 
     public static void main(String[] args) {
+        final GpioController gipo = GpioFactory.getInstance();
+
+        final GpioPinDigitalOutput[] pins = {
+                gipo.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW),
+                gipo.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW),
+        };
+
+        gipo.setShutdownOptions(true, PinState.LOW, pins);
+        GpioStepperMotorComponent motor = new GpioStepperMotorComponent(pins);
+
 
 
     }
