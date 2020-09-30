ea513a2d394da46cd570383c2a47efeed4ecbbc3
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-09-30 18:22:43 +0200

    Støtte for kamera

--- src/ImageHandler.java
+++ src/ImageHandler.java
@@ -1,4 +1,8 @@
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.Socket;
@@ -7,28 +11,40 @@ public class ImageHandler{
 
     private final static String HOST = "10.22.192.92";
     private final static int PORT = 42069;
+    private static Webcam webcam;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-            System.out.println("Connecting to server...");
-            Socket socket = new Socket(HOST, PORT);
-            System.out.println("Connected to server on: " + HOST + ":" + PORT);
+    public static void main(String[] args) throws IOException {
 
-            OutputStream outputStream = socket.getOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        System.out.println("Connecting to server...");
+        Socket socket = new Socket(HOST, PORT);
+        System.out.println("Connected to server on: " + HOST + ":" + PORT);
+
+
+        OutputStream outputStream = socket.getOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+        int imageTakeCounter = 0;
+        while(!socket.isClosed() & (imageTakeCounter < 10)) {
+            webcam = Webcam.getDefault();
+            for (Dimension supportedSize : webcam.getCustomViewSizes()) {
+                System.out.println(supportedSize.toString());
+            }
+            webcam.setViewSize(WebcamResolution.VGA.getSize());
+            webcam.open();
+            ImageIO.write(webcam.getImage(), "jpg", new File("C:\\Users\\gusta\\OneDrive\\Skrivebord\\images\\iamge" + imageTakeCounter + ".jpg"));
+            System.out.println("Image was taken!");
+            webcam.close();
+
 
             ImageObject imageObject = null;
-
-            int imageCount = 0;
-            while(!socket.isClosed() & (imageCount < 10)){
-                //File image = new File("C:\\Users\\Gustav\\Desktop\\images\\sau1.jpg");
-                File image = new File("/home/pi/image.jpg");
-                byte[] byteArray = imageToByteArray(image);
-                imageObject = new ImageObject("Image"+imageCount, image.length(), byteArray, "23.09.2020", "jpg");
-                objectOutputStream.writeObject(imageObject);
-                System.out.println("Image was sent!");
-                // Thread.sleep(2000);
-                imageCount++;
-            }
+            File image = new File("C:\\Users\\gusta\\OneDrive\\Skrivebord\\images\\iamge" + imageTakeCounter + ".jpg");
+            byte[] byteArray = imageToByteArray(image);
+            imageObject = new ImageObject("Image" + imageTakeCounter, image.length(), byteArray, "23.09.2020", "jpg");
+            objectOutputStream.writeObject(imageObject);
+            System.out.println("Image was sent!");
+            // Thread.sleep(2000);
+            imageTakeCounter++;
+        }
     }
 
     private static final byte[] imageToByteArray(File image) throws IOException {
