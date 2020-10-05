20c8a7f26e05b2b274f4ffdf833a07142ccdb671
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-10-05 18:11:28 +0200

    Made drive() and sleepMicro() in RunUGV

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,9 +1,10 @@
 import com.pi4j.io.gpio.*;
 
-public class Elevator{
+public class Elevator extends RunUGV{
     private static GpioPinDigitalOutput pul = null;
     private static GpioPinDigitalOutput dir = null;
     private static GpioPinDigitalOutput ena = null;
+
 
     public Elevator() {
     }
@@ -23,21 +24,21 @@ public class Elevator{
         drive(4000);
     }
 
-    private static void drive(int steps) throws InterruptedException {
-        for (int i = 0; i < steps; i++) {
-            pul.high();
-            sleepMicro(50);
-            pul.low();
-            sleepMicro(50);
-        }
-        Thread.sleep(1000);
-    }
+//    private static void drive(int steps) throws InterruptedException {
+//        for (int i = 0; i < steps; i++) {
+//            pul.high();
+//            sleepMicro(50);
+//            pul.low();
+//            sleepMicro(50);
+//        }
+//        Thread.sleep(1000);
+//    }
 
-    private static void sleepMicro(int delay) {
-        long initialTime = System.nanoTime();
-        long updatedTime = 0;
-        do {
-            updatedTime = System.nanoTime();
-        } while ((initialTime + delay * 1000) >= updatedTime);
-    }
+//    private static void sleepMicro(int delay) {
+//        long initialTime = System.nanoTime();
+//        long updatedTime = 0;
+//        do {
+//            updatedTime = System.nanoTime();
+//        } while ((initialTime + delay * 1000) >= updatedTime);
+//    }
 }
--- src/RunUGV.java
+++ src/RunUGV.java
@@ -1,2 +1,33 @@
+import com.pi4j.io.gpio.*;
+
 public class RunUGV {
+    private static GpioController gpioController = GpioFactory.getInstance();
+    private static GpioPinDigitalOutput pul = null;
+    private static GpioPinDigitalOutput dir = null;
+    private static GpioPinDigitalOutput ena = null;
+
+    public static void main(String[] args) throws InterruptedException {
+        pul = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+        dir = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
+        ena = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05, PinState.LOW);
+    }
+
+    public static void drive(int steps) throws InterruptedException {
+        for (int i = 0; i < steps; i++) {
+            pul.high();
+            sleepMicro(50);
+            pul.low();
+            sleepMicro(50);
+        }
+        Thread.sleep(1000);
+    }
+
+    public static void sleepMicro(int delay) {
+        long initialTime = System.nanoTime();
+        long updatedTime = 0;
+        do {
+            updatedTime = System.nanoTime();
+        } while ((initialTime + delay * 1000) >= updatedTime);
+    }
+
 }
