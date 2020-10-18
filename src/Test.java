1e5409434456ae506f7f913ba6aca47fd8b81bce
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-10-18 13:34:34 +0200

    Distance sensor test

--- src/Test.java
+++ src/Test.java
@@ -1,28 +1,49 @@
 import com.pi4j.io.gpio.*;
 
-public class Test{
+public class Test {
     private static GpioPinDigitalOutput pul = null;
+    private static GpioPinDigitalOutput trig = null;
+    private static GpioPinDigitalInput echo = null;
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         GpioController gpioController = GpioFactory.getInstance();
-        pul = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+        //pul = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+        trig = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+        echo = gpioController.provisionDigitalInputPin(RaspiPin.GPIO_04, PinPullResistance.PULL_DOWN);
 
-        int pulseWidth = 1000;
-        int steps = 10000;
-        System.out.println("Starting!");
-        for(int i = 0; i < steps; i++) {
-            pul.high();
-            sleepMicro(pulseWidth);
-            pul.low();
-            sleepMicro(20000 - pulseWidth);
-            if(steps > 500 && pulseWidth < 2000) {
-                pulseWidth++;
-            } else {
-                pulseWidth = 1000;
-            }
+        while (true) {
+                trig.high();
+                sleepMicro(10);
+                trig.low();
+
+                while (echo.isLow()) {
+                } //Wait until the ECHO pin gets HIGH
+
+                long startTime = System.nanoTime();
+                while (echo.isHigh()) {
+                } // Wait until the ECHO pin gets LOW
+                long endTime = System.nanoTime();
+
+                System.out.println("Distance: " + (((endTime - startTime) / 1000 / 2) / 29.1) + " cm");
+                sleepMicro(1000000); // Sleep for 1 000 000 microseconds = 1 seconds
         }
-        System.out.println("Done!");
+
+        // TEST FOR DC-MOTOR
+//        int pulseWidth = 1000;
+//        int steps = 10000;
+//        System.out.println("Starting!");
+//        for(int i = 0; i < steps; i++) {
+//            pul.high();
+//            sleepMicro(pulseWidth);
+//            pul.low();
+//            sleepMicro(20000 - pulseWidth);
+//            if(steps > 500 && pulseWidth < 2000) {
+//                pulseWidth++;
+//            } else {
+//                pulseWidth = 1000;
+//            }
+//        }
+//        System.out.println("Done!");
     }
 
     public static void sleepMicro(int delay) {
