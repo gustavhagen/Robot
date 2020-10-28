91c1b4ac69c887ea81d7f1857437c4a1cc93ae88
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-10-28 13:44:04 +0100

    Test

--- src/Test.java
+++ src/Test.java
@@ -3,27 +3,39 @@ import com.pi4j.io.gpio.*;
 public class Test {
     private static final long TIMEOUT = 23200;
     private static final double DISTANCE_CONSTANT = 170140e-9;
-    private static GpioPinDigitalOutput pul = null;
+    private static GpioPinDigitalOutput pul1 = null;
+    private static GpioPinDigitalOutput dir1 = null;
+    private static GpioPinDigitalOutput pul2 = null;
+    private static GpioPinDigitalOutput dir2 = null;
     private static GpioPinDigitalOutput trig = null;
     private static GpioPinDigitalInput echo = null;
     private static GpioPinDigitalOutput pwm = null;
     private static Servo servo;
-    private static StepperMotor stepperMotor;
+    private static StepperMotor stepperMotor1;
+    private static StepperMotor stepperMotor2;
     private static DriveMotor driveMotor;
     private static UltraSonicSensor ultraSonicSensor;
 
     public Test(){
         driveMotor = new DriveMotor(pwm);
+        stepperMotor1 = new StepperMotor(pul1, dir1);
+        stepperMotor2 = new StepperMotor(pul2, dir2);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         GpioController gpioController = GpioFactory.getInstance();
-        pwm = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_03);
-        trig = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01);
-        echo = gpioController.provisionDigitalInputPin(RaspiPin.GPIO_04, PinPullResistance.PULL_DOWN);
-        pul = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+        //pwm = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_03);
+        //trig = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+        //echo = gpioController.provisionDigitalInputPin(RaspiPin.GPIO_04, PinPullResistance.PULL_DOWN);
+        pul1 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
+        dir1 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
+        pul2 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
+        dir2 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
 
-        driveMotor.motorAct(1231, 1000);
+        stepperMotor1.moveUp(100);
+        stepperMotor2.moveUp(100);
+
+        //driveMotor.motorAct(1231, 1000);
 
 //        while (true) {
 //            System.out.println("Distance: " + getDistance() + " cm");
