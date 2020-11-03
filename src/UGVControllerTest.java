a4033ddc99c7b7f1f443464088fb62d60f3aed9f
Gustav Sørdal Hagen <gustavsh@stud.ntnu.no>
2020-11-03 20:12:39 +0100

    Arbeid 03.nov

--- src/UGVControllerTest.java
+++ src/UGVControllerTest.java
@@ -1,3 +1,5 @@
+import sun.java2d.loops.GraphicsPrimitive;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -8,9 +10,18 @@ public class UGVControllerTest implements Runnable {
     ImageHandler imageHandler;
     private ObjectOutputStream objectOutputStream;
     private ObjectInputStream objectInputStream;
+
     private boolean auto = false;
+    private volatile boolean manual = false;
+
+    private volatile boolean turnLeft = false;
+    private volatile boolean turnRight = false;
+    private volatile boolean driveForward = false;
+    private volatile boolean driveBackward = false;
 
     private int currentState;
+
+    Thread turnThread;
 
     public UGVControllerTest(Socket socket, ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream) {
         this.socket = socket;
@@ -19,10 +30,12 @@ public class UGVControllerTest implements Runnable {
     }
 
     public void run() {
+        turnThread = new Thread();
+        turnThread.start();
+
         try {
             Command command = new Command("UGV", 0, null, null);
             objectOutputStream.writeObject(command);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,89 +45,105 @@ public class UGVControllerTest implements Runnable {
                 Command command = (Command) objectInputStream.readObject();
                 System.out.println(command.getCommand());
 
-                if (command.getCommand().equals("directions") && !auto) {
+                if (command.getCommand() != null) {
                     boolean[] wasd = command.getWasd();
-                    System.out.println("Directions: w = " + wasd[0] + ", a = " + wasd[1] + ", s = " + wasd[2] + ", d = " + wasd[3]);
-                    System.out.println("Speed " + command.getValue());
-                }
-                if (command.getCommand().equals("start") && !auto) {
-                    System.out.println("Start automatic control");
-                    auto = true;
-                }
-                if (command.getCommand().equals("stop") && auto) {
-                    System.out.println("Stop automatic control");
-                    auto = false;
+                    switch (command.getCommand()) {
+                        case "manual":
+                            System.out.println("Manual mode...");
+                            printDriveDirection(command.getWasd());
+                            break;
+                        case "start":
+                            System.out.println("Started UGV in automatic control...");
+                            break;
+                        case "stop":
+                            System.out.println("Stop UGV in automatic control...");
+                            break;
+                    }
                 }
 
-                int captureStates = command.getValue();
-                boolean direction = false;
-                switch ("") {
-                    case "sne":         // IDLE
-                        System.out.println("UGV in IDLE-mode. Not driving.");
-                        changeStateTo(1);
-                        break;
-                    case "ani":         // LINE UP
-                        System.out.println("UGV is lining up for object...");
-                        changeStateTo(2);
-                        break;
-                    case "asd":         // CAPTURE
-                        System.out.println("UGV is going to capture 3 images in 3 different heights...");
-                        if (!direction) {
-                            Thread.sleep(500);
-                            captureStates(1);
-                            Thread.sleep(1500);
-                            captureStates(4);
-                            captureImageAndWait();
-                            Thread.sleep(1000);
-                            captureStates(2);
-                            Thread.sleep(1500);
-                            captureStates(4);
-                            captureImageAndWait();
-                            Thread.sleep(1000);
-                            captureStates(3);
-                            Thread.sleep(1500);
-                            captureStates(4);
-                            captureImageAndWait();
-                        }
-                        if (direction) {
-                            Thread.sleep(500);
-                            captureStates(3);
-                            Thread.sleep(1500);
-                            captureStates(4);
-                            captureImageAndWait();
-                            Thread.sleep(1000);
-                            captureStates(2);
-                            Thread.sleep(1500);
-                            captureStates(4);
-                            captureImageAndWait();
-                            Thread.sleep(1000);
-                            captureStates(1);
-                            Thread.sleep(1500);
-                            captureStates(4);
-                            captureImageAndWait();
-                        }
-                        System.out.println("Done capturing 3 images.");
-                        changeStateTo(3);
-                        break;
-                    case "asdc":          // DRIVE
-                        System.out.println("Continuing circle...");
-                        System.out.println("UGV moves a short distance...");
-                        direction = !direction;
+//                if (command.getCommand().equals("manual") && !auto) {
+//                    boolean[] wasd = command.getWasd();
+//                    System.out.println("Directions: w = " + wasd[0] + ", a = " + wasd[1] + ", s = " + wasd[2] + ", d = " + wasd[3]);
+//                    System.out.println("Speed " + command.getValue());
+//                }
+//                if (command.getCommand().equals("start") && !auto) {
+//                    System.out.println("Start automatic control");
+//                    auto = true;
+//                }
+//                if (command.getCommand().equals("stop") && auto) {
+//                    System.out.println("Stop automatic control");
+//                    auto = false;
+//                }
 
-                        break;
-                    case "agava":         // STOP
-                        System.out.println("UGV was stopped by operator.");
-                        break;
-                    default:              // DEFAULT
-                }
+//                int captureStates = command.getValue();
+//                boolean direction = false;
+//                switch (command.getCommand()) {
+//                    case "sne":         // IDLE
+//                        System.out.println("UGV in IDLE-mode. Not driving.");
+//                        changeStateTo(1);
+//                        break;
+//                    case "ani":         // LINE UP
+//                        System.out.println("UGV is lining up for object...");
+//                        changeStateTo(2);
+//                        break;
+//                    case "asd":         // CAPTURE
+//                        System.out.println("UGV is going to capture 3 images in 3 different heights...");
+//                        if (!direction) {
+//                            Thread.sleep(500);
+//                            captureStates(1);
+//                            Thread.sleep(1500);
+//                            captureStates(4);
+//                            captureImageAndWait();
+//                            Thread.sleep(1000);
+//                            captureStates(2);
+//                            Thread.sleep(1500);
+//                            captureStates(4);
+//                            captureImageAndWait();
+//                            Thread.sleep(1000);
+//                            captureStates(3);
+//                            Thread.sleep(1500);
+//                            captureStates(4);
+//                            captureImageAndWait();
+//                        }
+//                        if (direction) {
+//                            Thread.sleep(500);
+//                            captureStates(3);
+//                            Thread.sleep(1500);
+//                            captureStates(4);
+//                            captureImageAndWait();
+//                            Thread.sleep(1000);
+//                            captureStates(2);
+//                            Thread.sleep(1500);
+//                            captureStates(4);
+//                            captureImageAndWait();
+//                            Thread.sleep(1000);
+//                            captureStates(1);
+//                            Thread.sleep(1500);
+//                            captureStates(4);
+//                            captureImageAndWait();
+//                        }
+//                        System.out.println("Done capturing 3 images.");
+//                        changeStateTo(3);
+//                        break;
+//                    case "asdc":          // DRIVE
+//                        System.out.println("Continuing circle...");
+//                        System.out.println("UGV moves a short distance...");
+//                        direction = !direction;
+//
+//                        break;
+//                    case "agava":         // STOP
+//                        System.out.println("UGV was stopped by operator.");
+//                        break;
+//                    default:              // DEFAULT
+//                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        } //catch (InterruptedException e) {
+           // e.printStackTrace();
+        //}
     }
 
     private void captureStates(int state) {
@@ -131,6 +160,51 @@ public class UGVControllerTest implements Runnable {
             case 4:
                 System.out.println("Capturing image...");
                 break;
+        }
+    }
+
+    private void printDriveDirection(boolean[] wasd) {
+        int turnCounter = 0;
+        if (wasd[0]) {
+            if (wasd[3]) {
+                System.out.println("Both forward and backward keys are pressed...");
+            }
+            driveForward = true;
+            System.out.println("Driving forward...");
+        }
+        if (wasd[1]) {
+            if (wasd[2]) {
+                System.out.println("Both left and right keys are pressed...");
+            }
+            turnLeft = true;
+            System.out.println("Turning left...");
+        }
+        if (wasd[2]) {
+            if (wasd[1]) {
+                System.out.println("Both left and right keys are pressed...");
+            }
+            driveBackward = true;
+            System.out.println("Driving backward...");
+        }
+        if (wasd[3]) {
+            if (wasd[0]) {
+                System.out.println("Both forward and backward keys are pressed...");
+            }
+            turnRight = true;
+            System.out.println("Turning right...");
+        }
+
+        while (wasd[1] || wasd[3]) {
+            try {
+                if (turnCounter >= 0 && turnCounter <= 100) {
+                    turnLeft = true;
+                    System.out.println(turnCounter);
+                }
+                turnCounter++;
+                Thread.sleep(30);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
 
